0d61f4ec4aebfc2ab28366b82dcf628959064e7a
mherring2 <mherring2@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2012-10-23 08:58:25 +0000

    add garmin s/w
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/garmin@1041 043453e6-25df-4c13-81b0-d6a77d00cc6f

--- gmarine/src/Gmarine.java
+++ gmarine/src/Gmarine.java
@@ -1,3 +1,12 @@
+/* Copyright 2012 Malcolm Herring
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * For a copy of the GNU General Public License, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.io.IOException;
 import java.io.RandomAccessFile;
 

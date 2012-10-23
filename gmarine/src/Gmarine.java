149493081ca15d9bc0d4bbc57f30c4b716cd47f2
mherring2 <mherring2@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2012-10-23 08:57:03 +0000

    add garmin s/w
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/garmin@1040 043453e6-25df-4c13-81b0-d6a77d00cc6f

---
+++ gmarine/src/Gmarine.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class Gmarine {
+
+	public static void main(String[] args) {
+		final byte[] match = new byte[]{'G', 'A', 'R', 'M', 'I', 'N', ' ', 'T', 'R', 'E' };
+		int scan;
+		long fsize;
+		int tiles;
+		if (args.length == 1) {
+			try {
+				RandomAccessFile file = new RandomAccessFile(args[0], "rw");
+				fsize = file.length();
+				System.out.println("Processing " + fsize + " byte file...");
+				scan = tiles = 0;
+				while (file.getFilePointer() < fsize) {
+					if (file.readByte() == match[scan]) {
+						if (++scan == 10) {
+							tiles++;
+							scan = 0;
+							file.seek(file.getFilePointer() + 56);
+							file.writeByte(0x04);
+							file.writeByte(0x17);
+						}
+					} else {
+						scan = 0;
+					}
+				}
+				System.out.println(tiles + " tiles processed");
+				file.close();
+			} catch (IOException e) {
+				System.out.println("Unable to open " + args[0]);
+			}
+		} else {
+			System.out.println("Usage: java -jar gmarine <path/to/img/file>");
+		}
+	}
+
+}
---
+++ gsort/src/Gsort.java
@@ -0,0 +1,150 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class Gsort {
+	
+	public static void main(String[] args) {
+
+		class Way {
+			public ArrayList<String> tags;
+		}
+		
+		class Type {
+			public String key;
+			public String val;
+			public ArrayList<Way> objs;
+		}
+		
+		ArrayList<Type> types = new ArrayList<Type>();
+		
+		if (args.length == 2) {
+			try {
+				FileInputStream sort = new FileInputStream(args[0]);
+			  BufferedReader buff = new BufferedReader(new InputStreamReader(sort));
+			  String line;
+			  while ((line = buff.readLine()) != null) {
+			  	line = line.trim();
+			  	if (!line.isEmpty() && !line.startsWith("#") && line.contains("=")) {
+			  		String keyval[] = line.split("=");
+			  		if (keyval.length == 2) {
+			  			keyval[0] = keyval[0].trim();
+			  			keyval[1] = keyval[1].trim();
+			  			if (!keyval[0].isEmpty() && !keyval[1].isEmpty()) {
+			  				Type newtype = new Type();
+			  				newtype.key = keyval[0];
+			  				newtype.val = keyval[1];
+			  				newtype.objs = new ArrayList<Way>();
+			  				types.add(newtype);
+			  			} else {
+								System.out.println("Empty string: " + line);
+							  sort.close();
+				  			return;
+			  			}
+			  		} else {
+							System.out.println("Malformed data: " + line);
+						  sort.close();
+			  			return;
+			  		}
+			  	}
+			  }
+			  buff.close();
+			  sort.close();
+
+			} catch (IOException e) {
+				System.out.println("Failed to open " + args[0]);
+				return;
+			}
+			try {
+				File infile = new File(args[1]);
+				FileInputStream instream = new FileInputStream(infile);
+			  BufferedReader inbuff = new BufferedReader(new InputStreamReader(instream));
+				File outfile = new File("tmp");
+				FileOutputStream outstream = new FileOutputStream(outfile);
+			  BufferedWriter outbuff = new BufferedWriter(new OutputStreamWriter(outstream));
+			  String inline;
+			  Boolean inway = false;
+			  Type match = null;
+			  Way way = new Way();
+			  way.tags = new ArrayList<String>();
+			  while ((inline = inbuff.readLine()) != null) {
+			  	if (inway) {
+			  		if (inline.contains("</way>")) {
+			  			inway = false;
+			  		} else {
+				  		if (inline.contains("<tag")) {
+				  			int beg = inline.indexOf("k=", 0) + 2;
+				  			char quote = inline.charAt(beg);
+				  			int end = inline.indexOf(quote, beg+1);
+				  			String key = inline.substring(beg+1, end);
+				  			beg = inline.indexOf("v=", 0) + 2;
+				  			quote = inline.charAt(beg);
+				  			end = inline.indexOf(quote, beg+1);
+				  			String value = inline.substring(beg+1, end);
+				  			for (Type type : types) {
+				  				if (type.key.equals(key) && type.val.equals(value)) {
+				  					match = type;
+				  				}
+				  			}
+				  		}
+			  		}
+		  			way.tags.add(inline);
+		  			if (!inway) {
+		  				if (match != null) {
+		  					match.objs.add(way);
+		  				  way = new Way();
+		  				  way.tags = new ArrayList<String>();
+		  				} else {
+		  					while (way.tags.size() > 0) {
+		  						outbuff.write(way.tags.remove(0));
+							  	outbuff.newLine();
+		  					}
+		  				}
+		  			}
+			  	} else {
+			  		if (inline.contains("<way")) {
+			  			inway = true;
+			  			match = null;
+			  			way.tags.add(inline);
+			  		} else {
+			  			if (inline.contains("<relation") || inline.contains("</osm")) {
+			  				while (types.size() > 0) {
+			  					Type type = types.remove(0);
+			  					while (type.objs.size() > 0) {
+			  						Way item = type.objs.remove(0);
+				  					while (item.tags.size() > 0) {
+				  						outbuff.write(item.tags.remove(0));
+									  	outbuff.newLine();
+				  					}
+			  					}
+			  				}
+			  			}
+					  	outbuff.write(inline);
+					  	outbuff.newLine();
+			  		}
+			  	}
+			  }
+				System.out.println("Finished");
+				inbuff.close();
+				instream.close();
+		  	outbuff.flush();
+		  	outstream.flush();
+				outbuff.close();
+				outstream.close();
+				outfile.renameTo(infile);
+			} catch (IOException e) {
+				System.out.println("Unable to open " + args[1]);
+				return;
+			}
+		} else {
+			System.out.println("Usage: java -jar gsort.jar <path/to/sort/file> <path/to/osm/file>");
+		}
+	}
+
+}
